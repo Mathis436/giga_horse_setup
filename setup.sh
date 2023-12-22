7615025a22395e09b32c001d6b102f1f114913f2
Mathis436 <67963297+Mathis436@users.noreply.github.com>
2023-12-22 09:38:34 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,22 +1,26 @@
 sudo apt update -y && sudo apt upgrade -y
-sudo apt install nvidia-driver-535 unzip nvtop screen htop qemu-guest-agent nfs-common git python3-aionotify libgomp1 ocl-icd-libopencl1 rsync nano -y
-wget https://github.com/madMAx43v3r/chia-gigahorse/archive/refs/heads/master.zip
-unzip master.zip
-mv chia-gigahorse-master chia-gigahorse
+# sudo apt install nvidia-driver-535 unzip nvtop screen htop qemu-guest-agent nfs-common git python3-aionotify libgomp1 ocl-icd-libopencl1 rsync nano -y
+sudo apt install unzip screen htop qemu-guest-agent nfs-common git python3-aionotify libgomp1 ocl-icd-libopencl1 rsync nano -y
+git clone https://github.com/madMAx43v3r/chia-gigahorse.git
+wget https://github.com/madMAx43v3r/chia-gigahorse/releases/download/v2.1.3.giga26/chia-gigahorse-farmer-2.1.3.giga26-x86_64.tar.gz
+tar -xvf chia-gigahorse-farmer-2.1.3.giga26-x86_64.tar.gz
 echo "#!/usr/bin/env bash" >> cuplot.sh
 echo "cd /home/william/chia-gigahorse/cuda-plotter/linux/x86_64" >> cuplot.sh
 echo "./cuda_plot_k32 -n -1 -C 8 -t /mnt/temp/ -2 /mnt/local/ -f b437465a764326e215d9c92e4394f219a920f28a651d916d65b0af1ac5710ad894a5a9f9552d89006eee68fda043a211 -c xch12c7n4z6jaxhtqt4j4l694mdzadm363nu0u6e58p6fy8mz4p0z9lst6rqem" >> cuplot.sh
+sudo chown william:william /mnt
 chmod 777 cuplot.sh
-sudo mkdir /mnt/cache1
-sudo mkdir /mnt/cache2
-sudo mkdir /mnt/cache3
-sudo mkdir /mnt/cache4
-sudo mkdir /mnt/cache5
-echo "sudo mount -t nfs 192.168.137.56:/mnt/cache1 /mnt/cache1" >> mount.sh
-echo "sudo mount -t nfs 192.168.137.59:/mnt/cache2 /mnt/cache2" >> mount.sh
-echo "sudo mount -t nfs 192.168.137.59:/mnt/cache3 /mnt/cache3" >> mount.sh
-echo "sudo mount -t nfs 192.168.137.62:/mnt/cache4 /mnt/cache4" >> mount.sh
-echo "sudo mount -t nfs 192.168.137.62:/mnt/cache5 /mnt/cache5" >> mount.sh
+# sudo mkdir /mnt/cache1
+# sudo mkdir /mnt/cache2
+# sudo mkdir /mnt/cache3
+# sudo mkdir /mnt/cache4
+# sudo mkdir /mnt/cache5
+mkdir /mnt/cache
+sudo chown william:william /mnt/*
+echo "# sudo mount -t nfs 192.168.137.56:/mnt/cache1 /mnt/cache1" >> mount.sh
+echo "# sudo mount -t nfs 192.168.137.59:/mnt/cache2 /mnt/cache2" >> mount.sh
+echo "# sudo mount -t nfs 192.168.137.59:/mnt/cache3 /mnt/cache3" >> mount.sh
+echo "# sudo mount -t nfs 192.168.137.62:/mnt/cache4 /mnt/cache4" >> mount.sh
+echo "# sudo mount -t nfs 192.168.137.62:/mnt/cache5 /mnt/cache5" >> mount.sh
 chmod 777 mount.sh
 wget https://github.com/lmacken/plow/archive/refs/heads/main.zip
 unzip main.zip
